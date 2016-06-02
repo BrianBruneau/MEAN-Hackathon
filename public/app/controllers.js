03457f549e004eb166d68e12057e4a4bf27a45c4
BrianBruneau <Brian8ax@gmail.com>
2016-06-02 09:17:55 -0700

    controller http changes

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,17 +1,35 @@
 angular.module('chordCtrls', ['chordServices', 'chordDirective'])
 
-.controller('HomeCtrl', ['$scope', 'ChordFactory', function($scope, ChordFactory) {
-  console.log('good');
-  
-  $scope.getAll = function() {
-    console.log(ChordFactory);
+.controller('HomeCtrl', ['$scope', 'ChordFactory', '$http', function($scope, ChordFactory, $http) {
+  $scope.searchTerm = '';
+  $scope.movies = [];
 
-    ChordFactory.all(function success(data) {
-      console.log(data);
-    }, function error(data) {
-      console.log("error");
-    });
-}
+$scope.search = function() {
+  var req = {
+    url: "https://chords-api-app.herokuapp.com/guitar/:data",
+    method: 'GET',
+    params: {
+      s: $scope.searchTerm,
+    }
+  }
+
+  $http(req).then(function success(res) {
+    //do something with the response if successful
+    console.log(res);
+  }, function error(res) {
+    //do something if the response has an error
+    console.log(res);
+  })
+
+  $http.get(req).then(function success(res) {
+  if (res.status === 200) {
+    $scope.movies = res.data.Search;
+  }
+  console.log(res);
+}, function error(res) {
+  console.log(res);
+});
+};
 }])
 
 .controller('PianoCtrl', ['$scope', function($scope) {
--- public/app/services.js
+++ public/app/services.js
@@ -1,8 +1,7 @@
 angular.module('chordServices', ['ngResource'])
 
 .factory('ChordFactory', ['$resource', function($resource) {
-  return $resource('myApi.JSON', {}, {
-    all: {params: {id: undefined}, isArray: false},
-    get: {params: {id: 1}, isArray: false}
+  return $resource('https://chords-api-app.herokuapp.com/guitar/C/major', {}, {
+    all: {params: {id: undefined}, isArray: false}
   })
 }]);
